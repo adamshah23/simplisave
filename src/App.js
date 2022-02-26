dccc0032d58d0bde838182a768be2ad889eee4e4
adam <shah24@uw.edu>
2022-02-25 20:09:45 -0800

    progressing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import './App.css';
 import NavbarComp from './Header';
+import Camera from 'react-snap-pic';
+import { useState } from "react";
+import {BiCamera } from 'react-icons/bi'
 
 function App() {
+ 
+  const [showCamera, setShowCamera] = useState(false)
+ 
+
+  function takePicture (img) {
+    console.log(img)
+    setShowCamera(false)
+  }
+ 
   return (
     <div className="App">
       <NavbarComp/>
 
-      <div className="slogan">slogann</div>
+      <div className="slogan"> Simpily save money by donating and keeping track of your inventory! </div>
+      <div className= "text">Upload your files</div>
       <div className="upload-data">
-          <div className="pdf">pdf</div>
-          <div className="camera" >camera</div>
-      </div>
+          <div className="pdf">
+            <img src="https://cdn.icon-icons.com/icons2/1276/PNG/512/1497559948-24_85014.png" width="70" height="70"/>
+          </div>
 
+          <p> OR </p>
+
+          <div className="camera"> {showCamera && <Camera takePicture = {takePicture} />} 
+              <button onClick={showCamera}
+                style={{left:10, right:'auto'}}>
+                <BiCamera style={{height:65, width:65}} />
+            </button>
+          </div> 
+      </div>
     </div>
   );
 }
